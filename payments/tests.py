c0131bde6f3e2352a7bb2677acaebda02edb2fc4
Dibuji <emekzy10@gmail.com>
2025-05-24 11:15:14 +0100

    fixed test

--- payments/tests.py
+++ payments/tests.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from rest_framework.test import APITestCase, #APIClient
+from rest_framework.test import APITestCase #APIClient
 from rest_framework import status
 from .models import Payment
 
